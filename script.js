afd30cd2d7cfcc09b9a6a8a9c3dde0eeb0d61638
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-27 19:33:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const makeChange = (c) => {
   // your name here
+	const quarters = Math.floor(c/25);
+	const dimes = Math.floor((c%25)/10);
+	const nickels = Math.floor(((c%25)%10)/5);
+	const pennies = ((c%25)%10)%5;
+	return {"q": quarters, "d": dimes, "n": nickels, "p":pennies};
 };
 
 // Do not the change the code below
